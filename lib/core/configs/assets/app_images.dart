0cf9f7d981997a82ae898fe45d0149e088f948cc
Kimintrest <dianashebu@gmail.com>
2025-04-24 22:57:32 +0300

    proverka

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -3,7 +3,7 @@ class AppImages {
   static const String basePath = 'assets/images/';
 
   static const String introBG = '${basePath}colors.jpg';
-  static const String chooseModeBG= '${basePath}bailu.jpg';
+  static const String chooseModeBG= '${basePath}girl.jpg';
   static const String mango= '${basePath}Mongoz.png';
 
 }
